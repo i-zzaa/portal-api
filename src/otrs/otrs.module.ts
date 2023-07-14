be010a429158db388025e164f412596bb3cafc8b
c1321644 Isa Andressa Novaes Menezes <c1321644@interno.bb.com.br>
2023-07-14 17:49:15 -0300

    fix: ticket

--- src/otrs/otrs.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { OtrsService } from './otrs.service';
-
-@Module({
-  providers: [OtrsService],
-  exports: [OtrsService],
-})
-export class OtrsModule {}
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 
-import { OtrsModule } from 'src/otrs/otrs.module';
 import { PrismaService } from 'src/prisma.service';
 
 @Module({
-  imports: [OtrsModule],
   providers: [UsersService, PrismaService],
   exports: [UsersService],
 })
