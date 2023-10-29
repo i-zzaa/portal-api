1df5adc61db447073846f5a09c6233228a4e5657
c1321644 Isa Andressa Novaes Menezes <c1321644@interno.bb.com.br>
2023-10-29 12:27:51 -0300

    feat: limpando projeto

--- src/app.service.ts
+++ src/app.service.ts
@@ -10,19 +10,14 @@ export class AppService {
 
   async getInterfaceNetwork() {
     let address: any = '';
-    // Obtém a lista de interfaces de rede
     const networkInterfaces: any = os?.networkInterfaces();
 
-    // Itera sobre as interfaces de rede
     await Promise.all(
       Object.keys(networkInterfaces).map((interfaceName) => {
         const interfaces = networkInterfaces[interfaceName];
 
-        // Itera sobre os endereços da interface de rede
         interfaces.forEach((interfaceData) => {
-          // Verifica se é um endereço IPv4 e não é o endereço loopback
           if (interfaceData.family === 'IPv4' && !interfaceData.internal) {
-            console.log('Endereço IP da máquina:', interfaceData.address);
             address = interfaceData.address;
           }
         });
