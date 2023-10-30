c8799456a63cfccde9b3cedb82d08b81ab79a4e2
c1321644 Isa Andressa Novaes Menezes <c1321644@interno.bb.com.br>
2023-10-30 20:52:01 -0300

    feat: reply

--- src/util/format-date.ts
+++ src/util/format-date.ts
@@ -1,9 +1,5 @@
 import { format } from 'date-fns';
-import { utcToZonedTime } from 'date-fns-tz';
 
 export function formatDate(inputDateTime: any) {
-  const dataUTC = utcToZonedTime(new Date(inputDateTime), 'UTC');
-  const dataFormatada = format(dataUTC, 'dd/MM/yyyy HH:mm');
-
-  return dataFormatada;
+  return format(new Date(inputDateTime), 'dd/MM/yyyy HH:mm');
 }
