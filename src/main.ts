1d0edcdbe9106cad834843fb5145fc957f46d9e0
c1321644 Isa Andressa Novaes Menezes <c1321644@interno.bb.com.br>
2023-11-28 10:29:27 -0300

    Ajuste

--- src/main.ts
+++ src/main.ts
@@ -6,25 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
 
-  app.use(
-    session({
-      secret: process.env.SESSION_PRIVATE_KEY,
-      resave: false,
-      saveUninitialized: false,
-      cookie: { maxAge: 3600000 },
-    }),
-  );
-
-  app.use(passport.initialize());
-  app.use(passport.session());
-
-  app.enableCors({
-    origin: [
-      'http://127.0.0.1:5173',
-      'https://fbuots.hospedagemelastica.com.br/',
-    ],
-    credentials: true,
-  });
+  app.enableCors();
 
   await app.listen(3000);
 }
